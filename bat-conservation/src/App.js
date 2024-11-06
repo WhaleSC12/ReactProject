b1bb6ec30ff75135a9425ea93a05372cb1888e72
WhaleSC12 <WJHALE@email.sc.edu>
2024-11-06 17:59:50 -0500

    dsfesddddddddddddddd

--- bat-conservation/src/App.js
+++ bat-conservation/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Breeds from './pages/Breeds';
