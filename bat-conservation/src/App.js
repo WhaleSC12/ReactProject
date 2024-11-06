133e8a5abaa578cfb0f74e772390bd901fe05fee
WhaleSC12 <WJHALE@email.sc.edu>
2024-11-06 17:53:23 -0500

    dsfesddddddddd

--- bat-conservation/src/App.js
+++ bat-conservation/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Breeds from './pages/Breeds';
+import News from './pages/News';
+import Shop from './pages/Shop';
+import Safety from './pages/Safety';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/breeds" element={<Breeds />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/safety" element={<Safety />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
