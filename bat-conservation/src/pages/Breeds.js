6cafc845873d8ed97c57bc6f9c07afff19514e43
WhaleSC12 <WJHALE@email.sc.edu>
2024-11-07 09:28:10 -0500

    dsfesdsjedsdssdfsdkffsksdfsdflfdsfdsdsffdsfdsffdsfdsdsdsdffdsdsadfsdfddsdsddsfdsfdsffdsfdsdsdfdfdsfdsfdsffsdffdsf

--- bat-conservation/src/pages/Breeds.js
+++ bat-conservation/src/pages/Breeds.js
@@ -1,4 +1,58 @@
 // src/pages/Breeds.js
+import React, { useEffect, useState } from 'react'; 
+import batData from '../data/bats.json';
+import '../styles/styles.css';
+
+function Breeds() {
+  const [bats, setBats] = useState([]);
+  const [selectedBat, setSelectedBat] = useState(null);
+
+  useEffect(() => {
+    setBats(batData);
+  }, []);
+
+  const handleOpenModal = (bat) => {
+    setSelectedBat(bat);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedBat(null);
+  };
+
+  return (
+    <div className="breeds-container">
+      <h1>Bat Breeds</h1> {/* This header will push the images down */}
+      <div className="breeds-gallery">
+        {bats.map((bat) => (
+          <div key={bat._id} className="breeds-item" onClick={() => handleOpenModal(bat)}>
+            <img src={require(`../assets/${bat.img_name.slice(8)}`).default} alt={bat.name} />
+            <p>{bat.name}</p>
+          </div>
+        ))}
+      </div>
+
+      {selectedBat && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button className="close-button" onClick={handleCloseModal}>X</button>
+            <h2>{selectedBat.name}</h2>
+            <img src={require(`../assets/${selectedBat.img_name.slice(8)}`).default} alt={selectedBat.name} />
+            <p><strong>Conservation Status:</strong> {selectedBat.conservationStatus}</p>
+            <p><strong>Notable Features:</strong> {selectedBat.notable}</p>
+            <p><strong>Countries Found In:</strong> {selectedBat.countries.join(', ')}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Breeds;
+
+
+
+/*
+// src/pages/Breeds.js
 import React, { useState } from 'react';
 import '../styles/styles.css';
 
@@ -41,7 +95,7 @@ function Breeds() {
 
   return (
     <div className="breeds-container">
-      <h1>Bat Breeds</h1> {/* This header will push the images down */}
+      <h1>Bat Breeds</h1> {}
       <div className="breeds-gallery">
         {batData.map((bat) => (
           <div key={bat.id} className="breeds-item" onClick={() => handleOpenModal(bat)}>
@@ -69,7 +123,7 @@ function Breeds() {
 
 export default Breeds;
 
-
+*/
 
 
 /*
