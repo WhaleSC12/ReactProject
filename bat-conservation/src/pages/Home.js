b05ac4b1123249400755ce261cc1a1307742cc15
WhaleSC12 <WJHALE@email.sc.edu>
2024-11-07 06:50:33 -0500

    dsfesdsje

--- bat-conservation/src/pages/Home.js
+++ bat-conservation/src/pages/Home.js
@@ -5,11 +5,11 @@ import '../styles/styles.css';
 function Home() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const images = [
-    '/images/batanatomy.png',
-    '/images/canarylongearedbat.png',
-    '/images/floridabonnetedbat.jpg',
-    '/images/greaterbulldogbat.png',
-    '/images/flyingfoxbat.jpg'
+    '/assets/batanatomy.png',
+    '/assets/canarylongearedbat.png',
+    '/assets/floridabonnetedbat.jpg',
+    '/assets/greaterbulldogbat.png',
+    '/assets/flyingfoxbat.jpg'
   ];
 
   const changeSlide = (direction) => {
@@ -21,6 +21,12 @@ function Home() {
   return (
     <main className="container">
       <h1 className="heading">North American Bat Conservation Society</h1>
+      <p className="subheading">New: 
+        <a href="/news">Potential new cure for white-nose syndrome?</a> | 
+        <a href="/shop">New t-shirt!</a> | 
+        <a href="/breeds">New breed!</a>
+      </p>
+      
       <section className="carousel">
         <div className="carousel-container" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
           {images.map((image, index) => (
@@ -32,7 +38,12 @@ function Home() {
         <button className="carousel-btn prev" onClick={() => changeSlide(-1)}>&#10094;</button>
         <button className="carousel-btn next" onClick={() => changeSlide(1)}>&#10095;</button>
       </section>
-      <p className="footer-text">We are dedicated to preserving bat species...</p>
+      
+      <p className="footer-text-header">About us</p>
+      <p className="footer-text">
+        We are a for-profit organization dedicated to the preservation, conservation, and research of bat species in North America.
+        We are mainly based in the United States but have sister organizations in Canada, Mexico, and parts of Central America.
+      </p>
     </main>
   );
 }
