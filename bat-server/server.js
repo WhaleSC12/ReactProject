b950aeca6cf7aa2fb8765db1ec3254f2afc6b689
WhaleSC12 <WJHALE@email.sc.edu>
2024-11-10 14:36:29 -0500

    fsdfsfdsffsdffdsffsdf

--- bat-server/server.js
+++ bat-server/server.js
@@ -20,89 +20,89 @@ app.get('/api/bats', (req, res) => {
     res.json(batsData);
 });
 
-// Mock data or import your JSON file data
 const batsData = [
-  {
-    "_id": 1,
-    "name": "Big Brown Bat",
-    "img_name": "/assets/bigbrownbat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Can live over 20 years.",
-    "countries": ["Canada", "USA", "Mexico"]
-  },
-  {
-    "_id": 2,
-    "name": "Canary Long-eared Bat",
-    "img_name": "/assets/canarylongearedbat.png",
-    "conservationStatus": "Endangered",
-    "notable": "Has long ears adapted for echolocation.",
-    "countries": ["Canada", "USA"]
-  },
-  
-{
-    "_id": 3,
-    "name": "Eastern Red Bat",
-    "img_name": "easternredbat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Known for its reddish fur.",
-    "countries": ["Eastern USA", "Canada"]
-},
-{
-    "_id": 4,
-    "name": "Florida Bonneted Bat",
-    "img_name": "floridabonnetedbat.jpg",
-    "conservationStatus": "Critically Endangered",
-    "notable": "One of the rarest bats in North America.",
-    "countries": ["Florida, USA"]
-},
-{
-    "_id": 5,
-    "name": "Flying Fox Bat",
-    "img_name": "flyingfoxbat.jpg",
-    "conservationStatus": "Vulnerable",
-    "notable": "Largest species of bat.",
-    "countries": ["Not native to North America"]
-},
-{
-    "_id": 6,
-    "name": "Greater Bulldog Bat",
-    "img_name": "greaterbulldogbat.png",
-    "conservationStatus": "Least Concern",
-    "notable": "Feeds on fish using echolocation.",
-    "countries": ["Southern USA"]
-},
-{
-    "_id": 7,
-    "name": "Hoary Bat",
-    "img_name": "hoarybat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Migratory species, one of the most widespread in North America.",
-    "countries": ["USA, Canada, Mexico"]
-},
-{
-    "_id": 8,
-    "name": "Mexican Free-tailed Bat",
-    "img_name": "mexicanfreetailedbat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Known for its speed and agility in flight.",
-    "countries": ["USA, Mexico"]
-},
-{
-    "_id": 9,
-    "name": "Vampire Bat",
-    "img_name": "vampirebat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Feeds on blood, primarily livestock.",
-    "countries": ["Southern USA, Mexico"]
-},
-{
-    "_id": 10,
-    "name": "Yellow Bat",
-    "img_name": "yellowbat.jpg",
-    "conservationStatus": "Least Concern",
-    "notable": "Yellowish fur provides good camouflage.",
-    "countries": ["Southern USA"]
-}
+        {
+          "_id": 1,
+          "name": "Big Brown Bat",
+          "img_name": "assets/bigbrownbat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Can live over 20 years.",
+          "countries": ["Canada", "USA", "Mexico"]
+        },
+        {
+          "_id": 2,
+          "name": "Canary Long-eared Bat",
+          "img_name": "assets/canarylongearedbat.png",
+          "conservationStatus": "Endangered",
+          "notable": "Has long ears adapted for echolocation.",
+          "countries": ["Canada", "USA"]
+        },
+        {
+          "_id": 3,
+          "name": "Eastern Red Bat",
+          "img_name": "assets/easternredbat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Known for its reddish fur.",
+          "countries": ["Eastern USA", "Canada"]
+        },
+        {
+          "_id": 4,
+          "name": "Florida Bonneted Bat",
+          "img_name": "assets/floridabonnetedbat.jpg",
+          "conservationStatus": "Critically Endangered",
+          "notable": "One of the rarest bats in North America.",
+          "countries": ["Florida, USA"]
+        },
+        {
+          "_id": 5,
+          "name": "Flying Fox Bat",
+          "img_name": "assets/flyingfoxbat.jpg",
+          "conservationStatus": "Vulnerable",
+          "notable": "Largest species of bat.",
+          "countries": ["Not native to North America"]
+        },
+        {
+          "_id": 6,
+          "name": "Greater Bulldog Bat",
+          "img_name": "assets/greaterbulldogbat.png",
+          "conservationStatus": "Least Concern",
+          "notable": "Feeds on fish using echolocation.",
+          "countries": ["Southern USA"]
+        },
+        {
+          "_id": 7,
+          "name": "Hoary Bat",
+          "img_name": "assets/hoarybat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Migratory species, one of the most widespread in North America.",
+          "countries": ["USA, Canada, Mexico"]
+        },
+        {
+          "_id": 8,
+          "name": "Mexican Free-tailed Bat",
+          "img_name": "assets/mexicanfreetailedbat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Known for its speed and agility in flight.",
+          "countries": ["USA, Mexico"]
+        },
+        {
+          "_id": 9,
+          "name": "Vampire Bat",
+          "img_name": "assets/vampirebat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Feeds on blood, primarily livestock.",
+          "countries": ["Southern USA, Mexico"]
+        },
+        {
+          "_id": 10,
+          "name": "Yellow Bat",
+          "img_name": "assets/yellowbat.jpg",
+          "conservationStatus": "Least Concern",
+          "notable": "Yellowish fur provides good camouflage.",
+          "countries": ["Southern USA"]
+        }
+      
+      
 ];
 
 // Start the server
